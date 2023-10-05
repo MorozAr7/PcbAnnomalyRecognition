ddcd22a1925a2ee283dd8c78c783650a3caa9e98
Artem Moroz <69736160+MorozAr7@users.noreply.github.com>
2023-10-06 00:20:57 +0200

    Add files via upload

--- TrainConfig.py
+++ TrainConfig.py
@@ -1,13 +1,13 @@
 import torch
 
 LEARNING_RATE = 0.0025
-BATCH_SIZE = 20
+BATCH_SIZE = 8
 APPLY_AUGMENTATION = True
 
-DEVICE = "mps" if getattr(torch, 'has_mps', False) else 7 if torch.cuda.is_available() else "cpu"
+DEVICE = "mps" if getattr(torch, 'has_mps', False) else 5 if torch.cuda.is_available() else "cpu"
 
 NUM_MASK_SCALES = 3
-IMAGE_SIZE = 256
-MODEL_NAME = "Model256"
+IMAGE_SIZE = 384
+MODEL_NAME = "Model384"
 LossCoefficients = {"L1": 5, "SSIM": 1, "GradL1": 1, "GradSSIM": 1}
 
